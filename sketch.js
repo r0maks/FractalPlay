53a5b550eaf788a56c69f0b91c3a814994d0928c
r0maks <rrmaksimov@gmail.com>
2017-07-31 20:44:29 -0400

    so cool!

--- sketch.js
+++ sketch.js
@@ -2,32 +2,42 @@
 var angle = 0;
 var slider;
 
-
 function setup() {
     createCanvas(1000, 750);
     slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
 function draw() {
-    background(51);
+    background(41);
     angle = slider.value();
+
+
+    var seedLength = 150;
+
+
     stroke(240);
     translate(200, height);
-    branch(150);
+    branch(seedLength * angle);
 }
 
 
+function getRandomMultiplier(){
+    return random(0.8);
+}
+
 function branch(len) {
+  strokeWeight(random(5))
+  stroke(random(230), random(230), random(230));
   line(0, 0, 0, -len);
   translate(0, -len);
   if (len > 20) {
     push();
     rotate(angle);
-    branch(len * 0.75);
+    branch(len * getRandomMultiplier());
     pop();
     push();
     rotate(-angle);
-    branch(len * 0.75);
+    branch(len * getRandomMultiplier());
     pop();
   }
 }
